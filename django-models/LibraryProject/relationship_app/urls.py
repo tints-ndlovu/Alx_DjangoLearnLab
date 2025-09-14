2b9835e5a3e83eb95a5c81127ca3f7ae5267b413
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 20:30:52 +0200

    Task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
+    path("admin-role/", views.admin_view, name="admin_view"),
+    path("librarian-role/", views.librarian_view, name="librarian_view"),
+    path("member-role/", views.member_view, name="member_view"),
+
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,11 @@ from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+from .models import UserProfile
 
 # Function-based view: list all books
 def list_books(request):
@@ -24,16 +29,17 @@ from django.contrib import messages
 
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        # Use CustomUserCreationForm if role is included
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # auto login after register
+            role = form.cleaned_data.get("role")
+            if role:  # only update if role was selected
+                user.userprofile.role = role
+                user.userprofile.save()
+            login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect("home")  # adjust "home" if you have another landing page
+            return redirect("home")  # can point to any landing page
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
-
-
-
-
