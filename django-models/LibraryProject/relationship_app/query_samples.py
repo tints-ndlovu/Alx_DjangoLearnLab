07065e79527be77d457a66b6d25fd55e107e7d9f
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 15:50:28 +0200

    Fixed queries.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+books_by_author = Book.objects.filter(author__name="J.K. Rowling")
 
 # 2. List all books in a library
-def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+books_in_library = Library.objects.get(name="Central Library").books.all()
 
 # 3. Retrieve the librarian for a library
-def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+librarian_for_library = Librarian.objects.get(library__name="Central Library")
