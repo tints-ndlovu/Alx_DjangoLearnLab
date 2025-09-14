880da4202c6e75f27e22ffa7e2c80fd86d5a3548
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 23:52:48 +0200

    views import

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # Function-based view to list books with permission check
 @permission_required('bookshelf.can_view', raise_exception=True)
