f34344dbdbaa2cc97bdb8e865d35398983f99fde
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 23:45:42 +0200

    Bookshelf models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,11 +3,19 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create a book"),
+            ("can_delete", "Can delete a book"),
+            ("can_edit", "Can edit a book"),  # Optional if needed
+            ("can_view", "Can view a book"),  # Optional if needed
+        ]
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
     
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
