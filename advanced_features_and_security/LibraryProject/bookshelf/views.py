07ec1c5117377e30a0a35067f3603452ef306580
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 23:48:21 +0200

    Bookshelf views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# Function-based view to list books with permission check
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    # Retrieve all books from the database
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
