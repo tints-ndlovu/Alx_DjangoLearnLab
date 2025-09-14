7f9f9f6e8c60ab9da049734156c696ea88c9a30d
Tentswalo <tintswalotndlovu@gmail.com>
2025-09-14 23:16:15 +0200

    Error bookshelf

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import models
+from django.conf import settings
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
